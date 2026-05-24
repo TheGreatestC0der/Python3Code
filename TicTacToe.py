78b2fbcfaca30cdac4f55987682614ed4fbefbe0
TheGreatestC0der <padhyishaan021@gmail.com>
2026-05-24 14:52:46 -0700

    finished tic tac toe game

--- TicTacToe.py
+++ TicTacToe.py
@@ -95,6 +95,73 @@ def CREATOR(grid):
                     print(" | ", end = "")
         if row < 2:
             print("--+---+--")
-CREATOR(grid)
 
+def CHECK_WIN(grid):
+    count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row + 1 < len(grid) and grid[row + 1][col] == 1:
+                    if row + 2 < len(grid) and grid[row + 2][col] == 1:
+                        return 1
+                elif col + 1 < len(grid[row]) and grid[row][col + 1] == 1:
+                    if col + 2 < len(grid[row]) and grid[row][col + 2] == 1:
+                        return 1
+                elif row + 1 < len(grid) and col + 1 < len(grid[row]) and grid[row + 1][col + 1] == 1:
+                    if row + 2 < len(grid) and col + 2 < len(grid[row]) and grid[row + 2][col + 2] == 1:
+                        return 1
+                elif row - 1 > 0 and col + 1 < len(grid[row]) and grid[row - 1][col + 1] == 1:
+                    if row - 2 > 0 and col + 2 < len(grid[row]) and grid[row - 2][col + 2] == 1:
+                        return 1
+            elif grid[row][col] == 2:
+                if row + 1 < len(grid) and grid[row + 1][col] == 2:
+                    if row + 2 < len(grid) and grid[row + 2][col] == 2:
+                        return 2
+                elif col + 1 < len(grid[row]) and grid[row][col + 1] == 2:
+                    if col + 2 < len(grid[row]) and grid[row][col + 2] == 2:
+                        return 2
+                elif row + 1 < len(grid) and col + 1 < len(grid[row]) and grid[row + 1][col + 1] == 2:
+                    if row + 2 < len(grid) and col + 2 < len(grid[row]) and grid[row + 2][col + 2] == 2:
+                        return 2
+                elif row - 1 > 0 and col + 1 < len(grid[row]) and grid[row - 1][col + 1] == 2:
+                    if row - 2 > 0 and col + 2 < len(grid[row]) and grid[row - 2][col + 2] == 2:
+                        return 2
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] != " ":
+                count += 1
+    if count == 9:
+        return 0
 
+count = 0
+while True:
+    if count == 0:
+        CREATOR(grid)
+    player_1_row = int(input("Player 1, which row is the spot you want to mark in? "))
+    player_1_col = int(input("Which column is the spot you want to mark in? "))
+    if grid[player_1_row][player_1_col] == " ":
+        grid[player_1_row][player_1_col] = 1
+    else:
+        print("That spot's taken...")
+    CREATOR(grid)
+    if CHECK_WIN(grid) == 1:
+        print("Player 1 wins!")
+        break
+    elif CHECK_WIN(grid) == 0:
+        print("It's a tie!")
+        break
+
+    player_2_row = int(input("Player 2, which row is the spot you want to mark in? "))
+    player_2_col = int(input("Which column is the spot you want to mark in? "))
+    if grid[player_2_row][player_2_col] == " ":
+        grid[player_2_row][player_2_col] = 2
+    else:
+        print("That spot's taken...")
+    CREATOR(grid)
+    if CHECK_WIN(grid) == 2:
+        print("Player 2 wins!")
+        break
+    elif CHECK_WIN(grid) == 0:
+        print("It's a tie!")
+        break
+    count += 1
